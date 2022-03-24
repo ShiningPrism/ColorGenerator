d567d097e1399c0075adc9c552b31c82bc918f54
ShiningPrism <romantatartsev@gmail.com>
2022-03-24 15:05:21 +0200

    New updates.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,22 +11,35 @@ public class Main {
 }
 
 class color extends JFrame {
-    color() {
-        super("color");
-        Random randInt = new Random();
-        int value = 255;
 
-        int r = randInt.nextInt(value);
-        int g = randInt.nextInt(value);
-        int b = randInt.nextInt(value);
-        Color c = new Color(r, g, b);
+    color() {
+        super("ColorGenerator");
+        setSize(400, 400);
+
+        Color c = new Color(255,255,255);
 
         JPanel p = new JPanel();
+        JButton b1;
 
-        p.setBackground(c);
+        b1 = new JButton("Click to generate color");
+        b1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                b1.doClick(); {
+                    Random randInt = new Random();
+                    int value = 255;
+                    int r = randInt.nextInt(value);
+                    int g = randInt.nextInt(value);
+                    int b = randInt.nextInt(value);
 
-        setSize(200, 200);
+                    Color c = new Color(r, g, b);
+                    p.setBackground(c);
+                    b1.setText("R:"+r+" G:"+g+" B:"+b);
+                }
+            }
+        });
         add(p);
+        p.setBackground(c);
+        p.add(b1);
         show();
     }
 }
